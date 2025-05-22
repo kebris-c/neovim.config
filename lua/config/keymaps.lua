002640c85f8f3b677741b3b2f7bf48d1362de347
Kevin Bris <55791683+kebris-c@users.noreply.github.com>
2025-05-22 22:40:15 +0200

    Actualizar keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+local dap = require('dap')
 
 map('n', '<F12>', ':lua require("tools.keymap_helper").show_keymaps()<CR>', opts)
 map('n', '<F2>', ':NERDTreeToggle<CR>', opts)
@@ -9,4 +10,14 @@ map('n', '<F6>', ':!cc -Wall -Wextra -pedantic -g3 % -o a.out && echo "Compiled 
 map('n', '<F7>', ':lua package.loaded["tools.valgrind"] = nil; require("tools.valgrind").run_valgrind()<CR>', opts)
 map('n', '<F8>', ':lua package.loaded["tools.valgrind"] = nil; require("tools.valgrind").highlight_errors()<CR>', opts)
 map('n', '<F9>', ':lua require("tools.valgrind").clear_highlights()<CR>', opts)
+
+-- Lldb Mapping
+map('n', '<leader>dd', dap.continue, { desc = "Debug: Start/Continue" })
+map('n', '<leader>do', dap.step_over, { desc = "Debug: Step Over" })
+map('n', '<leader>di', dap.step_into, { desc = "Debug: Step Into" })
+map('n', '<leader>du', dap.step_out, { desc = "Debug: Step Out" })
+map('n', '<leader>db', dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+map('n', '<leader>dB', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, { desc = "Debug: Conditional Breakpoint" })
+map('n', '<leader>dr', dap.repl.open, { desc = "Debug: Open REPL" })
+map('n', '<leader>dl', dap.run_last, { desc = "Debug: Run Last" })
 
